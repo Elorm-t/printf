d48652adf52374537f0de581022adeeee2ffc91e
Elorm-t <delimits@ymail.com>
2023-03-25 09:45:41 -0700

    adding printf function

---
+++ printf.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _printf - ouputs format string
+ * @format: format string passed
+ *
+ * Return: number of characters outputed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list parg;
+	int a = 0;
+
+	gettype buff[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(parg, format);
+
+	a = handler(format, buff, parg);
+
+	va_end(parg);
+
+	return (a);
+}
